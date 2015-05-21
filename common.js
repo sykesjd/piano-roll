53bc8aca4db447cfe3e55b8560de3d655824a6df
sykesjd <jesse@dogwoodmicro.com>
2015-05-21 13:24:52 -0400

    styles refactoring

--- common.js
+++ common.js
@@ -22,7 +22,7 @@ function bar(value, reldat, part, maxright) {
 
 function diamond(value, reldat, part, maxright) {
 	y = (reldat[0] - value['pitch'] - 2)*84/reldat[1] + 3;
-	$('<div></div>').addClass('note').addClass('part2').css({"left":value['start']+190,"top":y+'vh'}).append($('<div></div>').css({'width':'0px','height':'0px','border-bottom':168/reldat[1]+'vh solid #666','border-left':'10px solid transparent','border-right':'10px solid transparent'})).append($('<div></div>').css({'width':'0px','height':'0px','border-top':168/reldat[1]+'vh solid #666','border-left':'10px solid transparent','border-right':'10px solid transparent'})).appendTo('body');
+	$('<div></div>').addClass('note').addClass('sp12').css({"left":value['start']+190,"top":y+'vh'}).append($('<div></div>').addClass('spu').css({'border-bottom-width':168/reldat[1]+'vh'})).append($('<div></div>').addClass('spd').css({'border-top-width':168/reldat[1]+'vh'})).appendTo('body');
 	return (value['end'] > maxright ? value['end'] : maxright);
 }
 
@@ -57,21 +57,21 @@ function conic(value, reldat, part, maxright) {
 function bowed(value, reldat, part, maxright) {
 	y = (reldat[0] - value['pitch'] - 1)*84/reldat[1] + 3;
 	width = value['end'] - value['start'] - 1;
-	$('<div></div>').addClass('note').addClass('sp' + part).css({"left":value['start']+200,"top":y+'vh'}).append($('<div></div>').css({'width':'0px','height':'0px','border-bottom':(84/reldat[1])+'vh solid ' + bcolor(part),'border-left':width/2+'px solid transparent','border-right':width/2+'px solid transparent'})).append($('<div></div>').css({'width':'0px','height':'0px','border-top':(84/reldat[1])+'vh solid ' + bcolor(part),'border-left':width/2+'px solid transparent','border-right':width/2+'px solid transparent'})).appendTo('body');
+	$('<div></div>').addClass('note').addClass('sp' + part).css({"left":value['start']+200,"top":y+'vh'}).append($('<div></div>').addClass('spu').css({'border-bottom-width':(84/reldat[1])+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'})).append($('<div></div>').addClass('spd').css({'border-top-width':(84/reldat[1])+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'})).appendTo('body');
 	return (value['end'] > maxright ? value['end'] : maxright);
 }
 
 function pluck(value, reldat, part, maxright) {
 	y = (reldat[0] - value['pitch'] - 1)*84/reldat[1] + 3;
 	width = value['end'] - value['start'] - 1;
-	$('<div></div>').addClass('note').addClass('sp' + part).css({"left":value['start']+200,"top":y+'vh'}).append($('<div></div>').css({'width':'0px','height':'0px','border-bottom':(56/reldat[1])+'vh solid ' + bcolor(part),'border-left':width/2+'px solid transparent','border-right':width/2+'px solid transparent'})).append($('<div></div>').css({'width':width,'height':(56/reldat[1])+'vh','background':bcolor(part)})).append($('<div></div>').css({'width':'0px','height':'0px','border-top':(56/reldat[1])+'vh solid ' + bcolor(part),'border-left':width/2+'px solid transparent','border-right':width/2+'px solid transparent'})).appendTo('body');
+	$('<div></div>').addClass('note').addClass('sp' + part).css({"left":value['start']+200,"top":y+'vh'}).append($('<div></div>').addClass('spu').css({'border-bottom-width':(56/reldat[1])+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'})).append($('<div></div>').addClass('part' + part).css({'width':width,'height':(56/reldat[1])+'vh'})).append($('<div></div>').addClass('spd').css({'border-top-width':(56/reldat[1])+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'})).appendTo('body');
 	return (value['end'] > maxright ? value['end'] : maxright);
 }
 
 function keyboard(value, reldat, part, maxright) {
 	y = (reldat[0] - value['pitch'] - 1)*84/reldat[1] + 3;
 	width = value['end'] - value['start'] - 1;
-	$('<div></div>').addClass('note').addClass('sp' + part).css({'width':'0px','left':value['start']+200,'height':'0px','top':y+'vh','border-bottom':(168/reldat[1])+'vh solid ' + bcolor(part),'border-left':width/2+'px solid transparent','border-right':width/2+'px solid transparent'}).appendTo('body');
+	$('<div></div>').addClass('note').addClass('sp' + part).addClass('spu').css({'left':value['start']+200,'top':y+'vh','border-bottom-width':(168/reldat[1])+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'}).appendTo('body');
 	return (value['end'] > maxright ? value['end'] : maxright);
 }
 
@@ -93,23 +93,6 @@ function orchnote(type, value, reldat, part, maxright) {
 		case 7: return keyboard(value, reldat, part, maxright);
 		case 8: return perc(value, reldat, part, maxright);
 		case 9: return perc(value, reldat, 12, maxright);
-	}
-}
-
-function bcolor(part) {
-	switch (part) {
-		case 0: return "#FF0000";
-		case 1: return "#007FFF";
-		case 2: return "#FFFF00";
-		case 3: return "#7F00FF";
-		case 4: return "#00FF00";
-		case 5: return "#FF007F";
-		case 6: return "#00FFFF";
-		case 7: return "#FF7F00";
-		case 8: return "#0000FF";
-		case 9: return "#7FFF00";
-		case 10: return "#FF00FF";
-		case 11: return "#00FF7F";
 	}
 }
 
