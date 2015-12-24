16ba2df7f465a90338aa6364691ed589ac93f42e
sykesjd <jesse@dogwoodmicro.com>
2015-12-24 11:45:58 -0500

    removed need to ask for instrument types; functionalized the code

--- MIDItoJSON.py
+++ MIDItoJSON.py
@@ -15,7 +15,7 @@ from MidiInFile import MidiInFile
 from MidiToText import MidiToText
 
 usage = """
-Usage: 
+Usage:
 	python MIDItoJSON.py <input_mid>
 
 Example:
@@ -45,52 +45,107 @@ class Tee(object):
 
 def main(input_filename):
 	# get info from user
+	(workname, composer, rolltype) = info()
+	# remove extension from filename
+	input_name = inputname(input_filename)
+	# read in midi as text and write to a temporary file
+	readmidi(input_filename,input_name)
+	# reformat events into a list of lists, where each sublist is a track; zeroth track holds tempi
+	(ntracks, division, tracks, parttypes) = converttoeventlists(input_name)
+	# merge tracks together into a unified event log
+	events = mergelogs(tracks)
+	# change times on events from MIDI clocks to microseconds
+	events = clockstomus(events, division)
+	# convert pairs of events into note objects, times from microseconds to pixels
+	notes = eventstonotes(ntracks, events)
+	# package all info into one dictionary, write to JSON file
+	jsonout(input_name, workname, composer, rolltype, notes, parttypes)
+	# print success message
+	print "JSON written to", input_name+'.json', "successfully!"
+	# remove temporary file
+	os.remove(input_name+"_temp.txt")
+
+# gets piece info from user
+def info():
 	workname = raw_input("Enter work name: ")
 	composer = raw_input("Enter composer name: ")
 	print "What type of roll do you want?"
-	print "0: Voice\t1: Song"
-	print "2: Solo\t2: Orchestra"
-	rolltype = raw_input("Enter type number: ")
-	# remove extension from filename
-	path, file = os.path.split(input_filename)
-	input_name = file.split('.')[0]
-	# read in midi as text and write to a temporary file
-	midiIn = MidiInFile(MidiToText(), input_filename)
-	with open(input_name+'_p1.txt','w') as part1o:
+	print "0: Voice 1: Song 2: Solo 3: Orchestra"
+	rolltype = int(raw_input("Enter type number: "))
+	return (workname, composer, rolltype)
+
+# strips filename of its extension
+def inputname(filename):
+	path, file = os.path.split(filename)
+	return file.split('.')[0]
+
+# uses MidiToText to read MIDI file
+def readmidi(filename,name):
+	midiIn = MidiInFile(MidiToText(), filename)
+	with open(name+'_temp.txt','w') as tempOut:
 		original = sys.stdout
-		sys.stdout = Tee(part1o)
+		sys.stdout = Tee(tempOut)
 		midiIn.read()
 		sys.stdout = original
-	# reformat events into a list of lists, where each sublist is a track; zeroth track holds tempi
-	with open(input_name+"_p1.txt",'r') as part1i:
-		line = dr.readline()
+
+# converts MidiToText output to a list of lists containing note and tempi events
+def converttoeventlists(name):
+	with open(name+"_temp.txt",'r') as tempIn:
+		line = tempIn.readline()
 		division = int(line.split(" ")[-1])
 		ntracks = int(line.split(' ')[3].split(',')[0])
 		tracks = []
 		tempi = []
+		parttypes = []
 		i = -1
-		for line in part1i:
+		for line in tempIn:
 			sections = line.split(' ')
 			if sections[0] == "Start":
 				tracks.append([])
+				parttypes.append(0 if i == -1 else parttypes[i])
 				i += 1
 			elif sections[0] == "tempo:":
 				tempi.append(["tempo",int(sections[1]),int(sections[3])])
+			elif sections[0] == "patch_change":
+				instrnum = int(sections[5], 16)
+				percchannel = int(sections[3], 16) == 9
+				parttypes[i] =  9 if percchannel else instrtype(instrnum)
 			elif sections[0] == "note_on" or sections[0] == "note_off":
 				tracks[i].append([sections[0],int(sections[5], 16),int(sections[-1])])
 	tracks.insert(0, tempi)
-	# get track info from user for orchestral pieces
-	if rolltype == 3:
-		print "You indicated this to be an orchestral work."
-		parttypes = []
-		print "Enter the type number of each part:"
-		print "\t0: Flute\t1: Double Reed (e.g. Oboe)\t2: Single Reed (e.g. Clarinet)"
-		print "\t3: Conical Bore (e.g. French horn)\t4:Cylindrical Bore (e.g. Trumpet)"
-		print "\t5: Bowed (e.g. Violin)\t6: Plucked (e.g. Guitar)\t7: Keyboard"
-		print "\t8: Pitched percussion\t9: Non-pitched percussion"
-		for i in range(ntracks):
-			parttypes.append(int(raw_input("Part " + str(i) + ": ")))
-	# merge tracks together into a unified event log
+	return (ntracks, division, tracks, parttypes)
+
+# determines instrument type from instrument number using General MIDI Level 1 spec
+def instrtype(gm1num):
+	if between(gm1num, 72, 79):
+		return 0
+	elif between(gm1num, 21, 23) or between(gm1num, 68, 70):
+		return 1
+	elif between(gm1num, 64, 67) or gm1num == 71 or gm1num == 109:
+		return 2
+	elif between(gm1num, 0, 7) or between(gm1num, 16, 20) or between(gm1num, 80, 95):
+		return 7
+	elif between(gm1num, 24, 39) or gm1num == 46 or between(gm1num, 104, 107):
+		return 6
+	elif between(gm1num, 8, 15) or gm1num == 47 or gm1num == 108 or gm1num == 114 or gm1num == 117:
+		return 8
+	elif between(gm1num, 40, 51) or gm1num == 55 or between(gm1num, 110, 111):
+		return 5
+	elif between(gm1num, 96, 103) or between(gm1num, 112, 127):
+		return 9
+	elif gm1num == 58 or gm1num == 60:
+		return 3
+	elif between(gm1num, 52, 63):
+		return 4
+	else: # instrument number not in GM1 spec; give rectangle shape
+		return 4
+
+# helper method: returns true if gm1num between low and high inclusive
+def between(gm1num,low,high):
+	return gm1num >= low and gm1num <= high
+
+# merges track event logs into one event log
+def mergelogs(tracks):
 	events = []
 	newlength = 0
 	for i in range(len(tracks)):
@@ -111,47 +166,47 @@ def main(input_filename):
 		if event[0] == "note_on" or event[0] == "note_off":
 			event.insert(0, str(fev-1))
 		events.append(event)
-	# reformat event log into text and output to another temporary file
-	with open(input_name+"_p2.txt",'w') as part2o:
-		for event in events:
-			part2o.write(" ".join(event) + '\n')
-	# change times on events from MIDI clocks to microseconds
-	with open(input_name+"_p2.txt",'r') as part2i:
-		with open(input_name+"_p3.txt",'w') as part3o:
-			part3o.write(input_name+'\n\n')
-			tempo = 0
-			temppoint = 0
-			temptemp = 0
-			for line in part2i:
-				sections = line.split(' ')
-				if sections[0] == 'tempo':
-					temptemp = temptemp + (int(sections[2])-temppoint)*tempo/division
-					temppoint = int(sections[2])
-					tempo = int(sections[1])
-				elif sections[0].isdigit():
-					sections[3] = str((int(sections[3])-temppoint)*tempo/division+temptemp)+'\n'
-					part3o.write(' '.join(sections))
-	# convert pairs of events into note objects, times from microseconds to pixels
+	return events
+
+# converts event time from MIDI clocks to microseconds
+def clockstomus(events, division):
+	newevents = []
+	tempo = 0
+	temppoint = 0
+	temptemp = 0
+	for event in events:
+		if event[0] == 'tempo':
+			temptemp = temptemp + (int(event[2])-temppoint)*tempo/division
+			temppoint = int(event[2])
+			tempo = int(event[1])
+		elif event[0].isdigit():
+			event[3] = str((int(event[3])-temppoint)*tempo/division+temptemp)+'\n'
+			newevents.append(event)
+	return newevents
+
+# converts events to note objects, times from microseconds to pixels
+def eventstonotes(ntracks, events):
 	notes = []
 	for i in range(ntracks):
 		notes.append([])
 	tempnon = []
-	with open(input_name+"_p3.txt",'r') as part3i:
-		for line in part3i:
-			sections = line.split(' ')
-			if sections[0].isdigit() and sections[1] == 'note_on':
-				tempnon.append(sections)
-			elif sections[0].isdigit() and sections[1] == 'note_off':
-				newnote = {}
-				newnote['pitch'] = int(sections[2])
-				for i in range(len(tempnon)):
-					if tempnon[i][0] == sections[0] and tempnon[i][2] == sections[2]:
-						break
-				newnote['start'] = int(tempnon[i][3]) / 1000000.0 * 60
-				tempnon.pop(i)
-				newnote['end'] = int(sections[3]) / 1000000.0 * 60
-				notes[int(sections[0])].append(newnote)
-	# package all info into one dictionary, write to JSON file
+	for event in events:
+		if event[0].isdigit() and event[1] == 'note_on':
+			tempnon.append(event)
+		elif event[0].isdigit() and event[1] == 'note_off':
+			newnote = {}
+			newnote['pitch'] = int(event[2])
+			for i in range(len(tempnon)):
+				if tempnon[i][0] == event[0] and tempnon[i][2] == event[2]:
+					break
+			newnote['start'] = int(tempnon[i][3]) / 1000000.0 * 60
+			tempnon.pop(i)
+			newnote['end'] = int(event[3]) / 1000000.0 * 60
+			notes[int(event[0])].append(newnote)
+	return notes
+
+# packages note objects into a dictionary, output the dict to JSON file
+def jsonout(name, workname, composer, rolltype, notes, parttypes):
 	notesobj = {}
 	notesobj['name'] = workname
 	notesobj['composer'] = composer
@@ -169,13 +224,8 @@ def main(input_filename):
 		trobj[i]['notes'] = []
 		for j in range(len(notes[i])):
 			trobj[i]['notes'].append(notes[i][j])
-	with open(input_name+'.json','w') as output:
+	with open(name+'.json','w') as output:
 		json.dump(notesobj, output)
-	print "JSON written to", input_name+'.json', "successfully!"
-	# remove temporary files
-	os.remove(input_name+"_p1.txt")
-	os.remove(input_name+"_p2.txt")
-	os.remove(input_name+"_p3.txt")
 
 if __name__ == '__main__':
 	try:
--- README.md
+++ README.md
@@ -17,26 +17,13 @@ You will be prompted to provide the name of the work, the composer of the work, 
 * 0: Voice roll - each track in the piece is treated as a voice in a fugue and is assigned a color to represent the voice
 * 1: Song roll - the first and second tracks are treated as melody and counter-melody and are given piano roll bars, while all other tracks are treated as accompaniment and are given diamond notes instead
 * 2: Solo roll - the color of the note depends on its pitch rather than its voice; good for works where voices are not clear-cut and a melody-accompaniment structure is not definable
-* 3: Orchestra roll - each track is treated like an instrument in an orchestra and given a different _shape_ (see comments in Roll.js); color depends on pitch
-
-If you indicate that you want an orchestra roll, in addition to being prompted for work and composer name, you will be prompted for the instrument type of each track, where the instrument type is denoted as an integer like so:
-
-* 0: for a flute instrument, such as flute or piccolo
-* 1: for a double reed instrument, such as oboe or bassoon
-* 2: for a single reed instrument, such as clarinet or recorder
-* 3: for a conical bore brass instrument, such as French horn or cornet
-* 4: for a cylindrical bore brass instrument, such as trumpet or trombone
-* 5: for a bowed string instrument, such as violin or cello
-* 6: for a plucked string instrument, such as harp or guitar
-* 7: for a keyboard instrument, such as piano or harpsichord
-* 8: for a pitched percussion instrument, such as timpani or marimba
-* 9: for an unpitched percussion instrument, such as snare or cymbal
+* 3: Orchestra roll - each track is treated like an instrument in an orchestra and given a different _shape_ depending on the type of instrument (see comments in Roll.js); color depends on pitch
 
 The output will be a JSON file where each track has an assigned number, instrument type, and list of note objects.
 
 ## Motify
 
-Motify is an optional stage in finalizing the JSON file for the roll page in which the user selects the notes in the piece which are parts of motifs or subjects. A roll is printed on the screen and notes are selected/deselected with mouse clicks and arrow keys, with an indication as to which motif the note belong via the select box.
+Motify is an optional stage in finalizing the JSON file for the roll page in which the user selects the notes in the piece which are parts of motifs or subjects. A roll is printed on the screen and notes are selected/deselected with mouse clicks and arrow keys, with an indication as to which motifs the note belongs via the select box.
 
 Upon finishing selecting all the appropriate notes, the user can then print the new JSON to the console, which the user can copy and paste over the old contents of the JSON file. The new JSON will be the same as the old JSON except each note object will also contain a list of motifs of which it is a part.
 
@@ -48,7 +35,6 @@ The roll page takes the JSON representation of a MIDI file and draws a piano rol
 
 This work is still in progress; future changes will include:
 
-* Remove the need to ask for instrument types in the conversion process for orchestral works: MIDI files provide instrument numbers which can be matched to the categories above
 * Matching MIDI to real performance: currently, the MIDI and MP3 files are coming from the same source; in the future, I would like to be able to align a MIDI file to an external performance - because MIDI Wagner clearly doesn't cut it
 * Lyrics on vocal notes
 * More pieces, of course
