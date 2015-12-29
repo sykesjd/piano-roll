1fa0681259a547870daa53cf47bfdaff9bdece3b
sykesjd <jesse@dogwoodmicro.com>
2015-12-29 11:22:30 -0500

    slight refactor

--- common.js
+++ common.js
@@ -32,30 +32,30 @@ function drawNote(i, j, value, part, type, style, tp, noteHeight) {
     y = (tp - value['pitch'] - 1) * noteHeight/2 + 3;
     // width decremented to space consecutive notes
     width = value['end'] - value['start'] - 1;
-    $note = $('<div></div>').addClass('note').attr('id',i+'-'+j);
+    $note = $('<div></div>').addClass('note').attr('id',i+'-'+j).css({"left":x,"top":y+'vh'});
     switch (type) {
         // flutes: fully rounded oval
-        case 0: $note.addClass('part' + part).css({'width':width,'left':x,'height':noteHeight+'vh','top':y+'vh','border-radius':width/2+'px / '+noteHeight/2+'vh'});break;
+        case 0: $note.addClass('part' + part).css({'width':width,'height':noteHeight+'vh','border-radius':width/2+'px / '+noteHeight/2+'vh'});break;
         // double reeds: flattened oval
-        case 1: $note.addClass('part' + part).css({'width':width,'left':x,'height':noteHeight+'vh','top':y+'vh','border-radius':width/4+'px / '+noteHeight/2+'vh'});break;
+        case 1: $note.addClass('part' + part).css({'width':width,'height':noteHeight+'vh','border-radius':width/4+'px / '+noteHeight/2+'vh'});break;
         // single reeds: half-rounded oval
-        case 2: $note.addClass('part' + part).css({'width':width,'left':x,'height':noteHeight+'vh','top':y+'vh','border-radius':width/3+'px / '+noteHeight/2+'vh'});break;
+        case 2: $note.addClass('part' + part).css({'width':width,'height':noteHeight+'vh','border-radius':width/3+'px / '+noteHeight/2+'vh'});break;
         // conical brass: rounded rectangle
-        case 3: $note.addClass('part' + part).css({'width':width,'left':x,'height':noteHeight+'vh','top':y+'vh','border-radius':noteHeight/2+'vh'});break;
+        case 3: $note.addClass('part' + part).css({'width':width,'height':noteHeight+'vh','border-radius':noteHeight/2+'vh'});break;
         // cylindrical brass or non-orchestral instrument: plain rectangle
-        case 4: $note.addClass('part' + part).css({'width':width,'left':x,'height':noteHeight+'vh','top':y+'vh'});break;
+        case 4: $note.addClass('part' + part).css({'width':width,'height':noteHeight+'vh'});break;
         // bowed strings: four-sided diamond
-        case 5: $note.addClass('sp' + part).css({"left":x,"top":y+'vh'}).append($('<div></div>').addClass('spu').css({'border-bottom-width':noteHeight/2+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'})).append($('<div></div>').addClass('spd').css({'border-top-width':noteHeight/2+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'}));break;
+        case 5: $note.addClass('sp' + part).append($('<div></div>').addClass('spu').css({'border-bottom-width':noteHeight/2+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'})).append($('<div></div>').addClass('spd').css({'border-top-width':noteHeight/2+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'}));break;
         // plucked strings: six-sided diamond
-        case 6: $note.addClass('sp' + part).css({"left":x,"top":y+'vh'}).append($('<div></div>').addClass('spu').css({'border-bottom-width':noteHeight/3+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'})).append($('<div></div>').addClass('part' + part).css({'width':width,'height':noteHeight/3+'vh'})).append($('<div></div>').addClass('spd').css({'border-top-width':noteHeight/3+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'}));break;
+        case 6: $note.addClass('sp' + part).append($('<div></div>').addClass('spu').css({'border-bottom-width':noteHeight/3+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'})).append($('<div></div>').addClass('part' + part).css({'width':width,'height':noteHeight/3+'vh'})).append($('<div></div>').addClass('spd').css({'border-top-width':noteHeight/3+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'}));break;
         // orchestral keyboard: up-pointing triangle
-        case 7: $note.addClass('sp' + part).addClass('spu').css({'left':x,'top':y+'vh','border-bottom-width':noteHeight+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'});break;
+        case 7: $note.addClass('sp' + part).addClass('spu').css({'border-bottom-width':noteHeight+'vh','border-left-width':width/2+'px','border-right-width':width/2+'px'});break;
         // pitched percussion: thin rectangle with a glow
-        case 8: $note.addClass('perc').addClass('part' + part).css({'left':x,'height':noteHeight+'vh','top':y+'vh'});break;
+        case 8: $note.addClass('perc').addClass('part' + part).css({'height':noteHeight+'vh'});break;
         // unpitched percussion: thin white rectangle with a glow
-        case 9: $note.addClass('perc').addClass('part12').css({'left':x,'height':noteHeight+'vh','top':y+'vh'});break;
+        case 9: $note.addClass('perc').addClass('part12').css({'height':noteHeight+'vh'});break;
         // accompaniment (for songs): small grey diamond
-        case 10: $note.addClass('sp12').css({"left":(x-5),"top":y+'vh'}).append($('<div></div>').addClass('spu').css({'border-bottom-width':noteHeight/2+'vh'})).append($('<div></div>').addClass('spd').css({'border-top-width':noteHeight/2+'vh'}));
+        case 10: $note.addClass('sp12').css({"left":(x-5)}).append($('<div></div>').addClass('spu').css({'border-bottom-width':noteHeight/2+'vh'})).append($('<div></div>').addClass('spd').css({'border-top-width':noteHeight/2+'vh'}));
     }
     if (value['motifs']) {
         $.each(value['motifs'],function(k,v){
