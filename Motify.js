fdfdb3a526db41fb691b73474773f3e919503774
sykesjd <jesse@dogwoodmicro.com>
2016-07-03 22:00:46 -0400

    Enforced stylistic consistencies

--- Motify.js
+++ Motify.js
@@ -1,33 +1,33 @@
 $(function(){
     // get piece name as it appears in the file system
     var piece = $(location).attr('href').split('?')[1];
-    $.getJSON("./JSON/"+piece+".json").done(function(data){
+    $.getJSON('./JSON/' + piece + '.json').done(function(data) {
         // calculate data necessary for proper note size and placement
         var reldat1 = reldata(data);
         // for each track in the piece
-        $.each(data['allnotes']['tracks'], function(i,val){
+        $.each(data['allnotes']['tracks'], function(i, val) {
             // get part number and type of track
             var reldat2 = parttype(val);
-            $.each(val['notes'], function(j, value){
+            $.each(val['notes'], function(j, value) {
                 // draw each note to screen
                 drawNote(i, j, value, reldat2[0], reldat2[1], reldat1[2], reldat1[0], reldat1[1]);
             });
         });
         $('.w').remove();
-        $('#info').html(data['name'] + ' - '+ data['composer']);
+        $('#info').html(data['name'] + ' - ' + data['composer']);
         $('.but').show();
         $('body').scrollLeft(0);
         // core of motify: allow user to select motifs and print resulting JSON to console
         var newdata = data;
-        $.each(data['allnotes']['tracks'], function(i,val){
-            $.each(val['notes'], function(j,value){
+        $.each(data['allnotes']['tracks'], function(i, val) {
+            $.each(val['notes'], function(j, value) {
                 value['motifs'] = [];
             });
         });
         var reset = newdata;
         var $curr;
-        $('.note').click(function(){
-            $(this).toggleClass('motif'+$("#motifbox").val());
+        $('.note').click(function() {
+            $(this).toggleClass('motif' + $('#motifbox').val());
             $curr = $(this);
         });
         $(document).keydown(function(e) {
@@ -41,9 +41,9 @@ $(function(){
                 $curr = $curr.prev();
             }
         });
-        $('#ptc').click(function(){
+        $('#ptc').click(function() {
             for (var k = 0; k < 12; k++) {
-                $('.motif'+k).each(function(){
+                $('.motif' + k).each(function() {
                     var index = $(this).attr('id').split('-');
                     newdata['allnotes']['tracks'][parseInt(index[0])]['notes'][parseInt(index[1])]['motifs'].push(k);
                 });
@@ -51,7 +51,7 @@ $(function(){
             console.log(JSON.stringify(newdata));
             newdata = reset;
         });
-    }).fail(function(){
-        $('.w').html("Error loading data file; check your browser settings");
+    }).fail(function() {
+        $('.w').html('Error loading data file; check your browser settings');
     });
 });
--- Roll.js
+++ Roll.js
@@ -1,13 +1,13 @@
-$(function(){
+$(function() {
     // get piece name as it appears in the file system
     var piece = $(location).attr('href').split('?')[1];
-    $.getJSON("./JSON/"+piece+".json").done(function(data){
+    $.getJSON('./JSON/' + piece + '.json').done(function(data) {
         // calculate data necessary for proper note size and placement
         var reldat1 = reldata(data);
         // maxright used to determine how far right the roll goes in order to correctly place an invisible dummy note which will allow the window to scroll past the end
         var maxright = 0;
         // for each track in the piece
-        $.each(data['allnotes']['tracks'], function(i,val){
+        $.each(data['allnotes']['tracks'], function(i, val) {
             // get part number and type of track
             var reldat2 = parttype(val);
             $.each(val['notes'], function(j, value){
@@ -18,23 +18,23 @@ $(function(){
             });
         });
         // move dummy note, show interface elements
-        $('#scrollallow').css({'left':maxright});
+        $('#scrollallow').css({'left': maxright});
         $('.w').remove();
-        $('audio').attr('src',"./MP3/"+piece+".mp3");
-        $('#info').html(data['name'] + ' - '+ data['composer']);
+        $('audio').attr('src', './MP3/' + piece + '.mp3');
+        $('#info').html(data['name'] + ' - ' + data['composer']);
         $('#now').show();
         // roll animates at 60pps while audio is playing
-        var timer = window.setInterval(function(){
-            $('body').scrollLeft($('audio')[0].currentTime*60);
+        var timer = window.setInterval(function() {
+            $('body').scrollLeft($('audio')[0].currentTime * 60);
         }, 1000/60.0);
-        $('audio').on('pause',function(){
+        $('audio').on('pause', function() {
             // pause animation, unhide info
-            $('#info').animate({right:'10px'},500);
+            $('#info').animate({right: '10px'}, 500);
         });
-        $('audio').on('play',function(){
-            $('#info').animate({right:'-2000px'},500);
+        $('audio').on('play', function() {
+            $('#info').animate({right: '-2000px'}, 500);
         });
-    }).fail(function(){
-        $('.w').html("Error loading data file; check your browser settings");
+    }).fail(function() {
+        $('.w').html('Error loading data file; check your browser settings');
     });
 });
