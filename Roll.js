0ce5c93884be2f47221e8254915b6c474cbfdd9b
sykesjd <jesse@dogwoodmicro.com>
2016-08-29 19:21:11 -0400

    motify and lyricize now use worker

--- Roll.js
+++ Roll.js
@@ -2,34 +2,13 @@
  * Roll.js: draws an animated piano roll synced to an MP3
  */
 $(() => {
- 	if (typeof(Worker) !== 'undefined') {
- 		// get piece name as it appears in the file system
- 		let piece = $(location).attr('href').split('?')[1];
-        $.getJSON('./JSON/' + piece + '.json').done((data) => {
-            let w = new Worker('NoteWorker.js');
-     		w.onmessage = (event) => {
-     			if (event.data.meta) {
-     				$('#info').html(event.data.meta.name + ' - ' + event.data.meta.composer);
-     				w.terminate();
-     				$('.w').remove();
-     				$('audio').attr('src', './MP3/' + piece + '.mp3');
-     		        $('#now').show();
-     		        // roll animates at 60pps while audio is playing
-     		        let timer = window.setInterval(() => $('body').scrollLeft($('audio')[0].currentTime * 60), 1000/60.0);
-     		        // hide/unhide info on play/pause
-     		        $('audio').on('pause', () => $('#info').animate({right: '10px'}, 500));
-     		        $('audio').on('play', () => $('#info').animate({right: '-2000px'}, 500));
-     			} else if (event.data.maxright) {
-     				$('#scrollallow').css({'left': event.data.maxright});
-     			} else {
-     				$(event.data).appendTo('#roll');
-     			}
-     		};
-     		w.postMessage(data);
-        }).fail(() => {
-            $('.w').html('Error loading data file; check your browser settings');
-        });
- 	} else {
- 		$('#result').text('Your browser does not support Web Workers');
- 	}
+    getRoll((piece, data) => {
+        $('audio').attr('src', './MP3/' + piece + '.mp3');
+        $('#now').show();
+        // roll animates at 60pps while audio is playing
+        let timer = window.setInterval(() => $('body').scrollLeft($('audio')[0].currentTime * 60), 1000/60.0);
+        // hide/unhide info on play/pause
+        $('audio').on('pause', () => $('#info').animate({right: '10px'}, 500));
+        $('audio').on('play', () => $('#info').animate({right: '-2000px'}, 500));
+    });
 });
