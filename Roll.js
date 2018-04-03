544f6351bd937254454fe19a93525cc3bb0dc201
sykesjd <jesse@dogwoodmicro.com>
2018-04-03 19:09:43 -0400

    Fix animation

--- Roll.js
+++ Roll.js
@@ -6,7 +6,7 @@ $(() => {
     getRoll((piece, data) => {
         $('#now').show();
         $('audio').attr('src', './MP3/' + piece + '.mp3');
-        let timer = window.setInterval(() => $('body').scrollLeft($('audio')[0].currentTime * PPS), 1000.0/PPS);
+        let timer = window.setInterval(() => $('#roll').css('left', $('audio')[0].currentTime * PPS * -1), 1000.0/PPS);
         $('audio').on('pause', () => $('#info').animate({right: '10px'}, 500));
         $('audio').on('play', () => $('#info').animate({right: '-2000px'}, 500));
     });
